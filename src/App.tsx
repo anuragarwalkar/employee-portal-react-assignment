d82c55340b228baafe492eb53f6010ed5e7e12fb
Anurag <aarwalka@cisco.com>
2020-10-11 19:28:27 +0530

    added lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Dashboard from "./containers/dashboard/dashboard";
 import Layout from "./hoc/layout/layout";
-import Auth from "./containers/auth/auth";
 import { RootState } from "./models/rootState.model";
 import { connect } from 'react-redux';
 import styles from './App.module.css';
+import { CircularProgress } from "@material-ui/core";
+
+const Spinner = () => <div className={styles.absoluteCenter}><CircularProgress size={100} /></div> 
+
+// Lazy Imports
+const Auth = lazy(() => import('./containers/auth/auth'));
+const Dashboard = lazy(() => import('./containers/dashboard/dashboard'));
 
 const App = ({isAuthenticated}: {isAuthenticated: boolean}) => {
 
@@ -16,10 +21,12 @@ const App = ({isAuthenticated}: {isAuthenticated: boolean}) => {
   return (
     <BrowserRouter>
       <Layout className={styles.app}>
+        <Suspense fallback={<Spinner />}>
         <Switch>
           <Route path="/" exact render={() => routeGuard(Dashboard)} />
           <Route path="/auth" component={Auth}/>
         </Switch>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
