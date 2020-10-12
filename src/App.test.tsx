4b34f3e1bbd4f499eccf3e08ff3eb66579b63dbe
Anurag <aarwalka@cisco.com>
2020-10-12 08:00:58 +0530

    added basic app test and test workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,18 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { App } from "./App";
+import { configure, shallow, ShallowWrapper } from "enzyme";
+import Enzyme from "enzyme-adapter-react-16";
 
-test('Render <App/>', () => {
-  expect('').toBe('');
+configure({ adapter: new Enzyme() });
+
+describe("<App />", () => {
+  it("should render <App /> if user is not authenticated", () => {
+    const wrapper: ShallowWrapper = shallow(<App isAuthenticated={false} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render <App /> if user is not authenticated", () => {
+    const wrapper: ShallowWrapper = shallow(<App isAuthenticated={true} />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
